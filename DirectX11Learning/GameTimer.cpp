b59722e2284088763e0df6b66286ef468378eb56
NguyenTienLong <77033383+BuCiuPi@users.noreply.github.com>
2024-07-22 20:55:38 +0700

    Init Project and GameTimer

---
+++ DirectX11Learning/GameTimer.cpp
@@ -0,0 +1,97 @@
+#include "GameTimer.h"
+#include "windows.h"
+
+GameTimer::GameTimer()
+{
+	mSecondsPerCount = 0.0;
+	mDeltaTime = -1.0;
+	mBaseTime = 0;
+	mPausedTime = 0;
+	mPrevTime = 0;
+	mCurrTime = 0;
+	mStopped = false;
+
+	__int64 countsPerSec;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
+	mSecondsPerCount = 1.0 / (double)countsPerSec;
+}
+
+float GameTimer::GetTotalTime() const
+{
+	if (mStopped)
+	{
+		return (float)(((mStopTime - mPausedTime) - mBaseTime) * mSecondsPerCount);
+	}
+	else
+	{
+		return (float)(((mCurrTime - mPausedTime) - mBaseTime) * mSecondsPerCount);
+	}
+}
+
+float GameTimer::GetDeltaTime() const
+{
+	return (float)mDeltaTime;
+}
+
+void GameTimer::Reset()
+{
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+	mBaseTime = currTime;
+	mPrevTime = currTime;
+	mStopTime = 0;
+	mStopped = false;
+}
+
+void GameTimer::Start()
+{
+	__int64 startTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
+
+	if (mStopped)
+	{
+		mPausedTime += (startTime - mStopTime);
+
+		mPrevTime = startTime;
+		mStopTime = 0;
+		mStopped = false;
+	}
+}
+
+void GameTimer::Stop()
+{
+	if (!mStopped)
+	{
+		__int64 currTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+		mStopTime = currTime;
+		mStopped = true;
+	}
+
+
+}
+
+void GameTimer::Tick()
+{
+	if (mStopped)
+	{
+		mDeltaTime = 0.0;
+		return;
+	}
+
+	_int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+	mCurrTime = currTime;
+
+	mDeltaTime = (mCurrTime - mPrevTime) * mSecondsPerCount;
+
+	mPrevTime = mCurrTime;
+
+	//processor goes into a power save mode or we get shuffled to another processor, then mDeltaTime can be negative.
+	if (mDeltaTime < 0.0)
+	{
+		mDeltaTime = 0.0;
+	}
+}
